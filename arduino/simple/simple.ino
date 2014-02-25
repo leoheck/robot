1c65d3f87afae9e61ee70519ac5037a4aec4a613
Leandro Heck <leoheck@gmail.com>
2014-02-25 19:20:10 -0300

    Added verbosity flag and update command keys

--- arduino/simple/simple.ino
+++ arduino/simple/simple.ino
@@ -20,9 +20,16 @@
 #define c_key 99
 #define v_key 118
 #define q_key 113
+#define z_key 90
 #define space 32
 #define minus 45
 #define equal 61
+
+// Arrows (last command)
+#define arrow_up   65
+#define arrow_down 66
+#define arrow_left 68
+#define arrow_righ 67
 
 // Robot commands
 #define move_left_cmd     a_key
@@ -33,18 +40,22 @@
 #define decr_speed_cmd    minus
 #define incr_speed_cmd    equal
 #define debug_cmd         x_key
+#define debug_toggle      z_key
 
 int serial_command = 0;
 
 // M1 initial conditions
-int m1_speed = 90;
+int m1_speed = 115;
 bool m1_a1 = LOW;
 bool m1_a2 = LOW;
 
 // M2 initial conditions
-int m2_speed = 90;
+int m2_speed = 115;
 bool m2_a1 = LOW;
 bool m2_a2 = LOW;
+
+// Verbosity mode (default verbose)
+int verbose = 1;
 
 void setup()
 {
@@ -71,6 +82,7 @@ void setup()
 	// Serial configuration
 	Serial.begin(230400);
 	Serial.println("[ARDUINO] Robot ready!");
+	Serial.println("[ARDUINO] Hit z key to toogle verbosity mode.");
 }
 
 void loop()
@@ -85,79 +97,66 @@ void loop()
 		// Decodifica os valores
 		switch(serial_command)
 		{
+
+			case arrow_up:
 			case move_forward_cmd: {
-				Serial.println("[ARDUINO] Moving forward...");
-				move_forward();
+				move_forward(verbose);
 				break;
 			}
 
+			case arrow_down:
 			case move_backward_cmd: {
-				Serial.println("[ARDUINO] Moving backward...");
-				move_backward();
+				move_backward(verbose);
 				break;
 			}
 
+			case arrow_left:
 			case move_left_cmd: {
-				Serial.println("[ARDUINO] Turing left");
-				turn_left();
+				turn_left(verbose);
 				break;
 			}
 
+			case arrow_righ:
 			case move_right_cmd: {
-				Serial.println("[ARDUINO] Turning right");
-				turn_right();
+				turn_right(verbose);
 				break;
 			}
 
 			case break_cmd: {
-				Serial.println("[ARDUINO] Breaking");
-				stop();
+				stop(verbose);
 				break;
 			}
 
 			case decr_speed_cmd: {
-				Serial.println("[ARDUINO] Decreasing speed: ");
-				speed_down();
+				speed_down(verbose);
 				break;
 			}
 
 			case incr_speed_cmd: {
-				Serial.println("[ARDUINO] Increasing speed: ");
-				speed_up();
+				speed_up(verbose);
 				break;
 			}
 
 			case debug_cmd: {
-				Serial.println("[ARDUINO] Stop");
-				//debug_motors();
+				debug_motors(verbose);
+				break;
+			}
+
+
+			case debug_toggle: {
+				if(verbose) verbose = 0;
+				else verbose = 1;
 				break;
 			}
 		}
 	}
 }
 
-// Show motor pin states
-void debug_motors()
+void move_forward(int verbose)
 {
-	Serial.println("");
-	Serial.print("   M1 EN: ");
-	Serial.println(m1_speed);
-	Serial.print("   M1 A1: ");
-	Serial.println(m1_a1);
-	Serial.print("   M1 A2: ");
-	Serial.println(m1_a2);
-	Serial.println("");
-	Serial.print("   M2 EN: ");
-	Serial.println(m2_speed);
-	Serial.print("   M2 A1: ");
-	Serial.println(m2_a1);
-	Serial.print("   M2 A2: ");
-	Serial.println(m2_a2);
-	Serial.println("");
-}
+	if(verbose)
+		Serial.println("[ARDUINO] Moving forward...");
 
-void move_forward()
-{
 	m1_a1 = HIGH;
 	m1_a2 = LOW;
 	m2_a1 = HIGH;
@@ -169,8 +168,11 @@ void move_forward()
 	digitalWrite(M2_A2_PIN, m2_a2);
 }
 
-void move_backward()
+void move_backward(int verbose)
 {
+	if(verbose)
+		Serial.println("[ARDUINO] Moving backward...");
+
 	m1_a1 = LOW;
 	m1_a2 = HIGH;
 	m2_a1 = LOW;
@@ -182,8 +184,11 @@ void move_backward()
 	digitalWrite(M2_A2_PIN, m2_a2);
 }
 
-void turn_left()
+void turn_left(int verbose)
 {
+	if(verbose)
+		Serial.println("[ARDUINO] Turing left");
+
 	m1_a1 = HIGH;
 	m1_a2 = LOW;
 	m2_a1 = LOW;
@@ -195,8 +200,11 @@ void turn_left()
 	digitalWrite(M2_A2_PIN, m2_a2);
 }
 
-void turn_right()
+void turn_right(int verbose)
 {
+	if(verbose)
+		Serial.println("[ARDUINO] Turning right");
+
 	m1_a1 = LOW;
 	m1_a2 = LOW;
 	m2_a1 = HIGH;
@@ -208,8 +216,11 @@ void turn_right()
 	digitalWrite(M2_A2_PIN, m2_a2);
 }
 
-void stop()
+void stop(int verbose)
 {
+	if(verbose)
+		Serial.println("[ARDUINO] Breaking");
+
 	m1_a1 = LOW;
 	m1_a2 = LOW;
 	m2_a1 = LOW;
@@ -221,7 +232,7 @@ void stop()
 	digitalWrite(M2_A2_PIN, m2_a2);
 }
 
-void speed_up()
+void speed_up(int verbose)
 {
 	if(m1_speed < 255)
 	{
@@ -229,17 +240,19 @@ void speed_up()
 		m2_speed = m2_speed + 1;
 		if(m1_speed < 0) m1_speed = 0;
 		if(m2_speed < 0) m2_speed = 0;
-		Serial.print("[ARDUINO] Increasing speed: ");
-		Serial.print(((float)m1_speed * 100) / 255);
-		Serial.print("% [");
-		Serial.print(m1_speed);
-		Serial.println("]");
+		if(verbose) {
+			Serial.print("[ARDUINO] Increasing speed: ");
+			Serial.print(((float)m1_speed * 100) / 255);
+			Serial.print("% [");
+			Serial.print(m1_speed);
+			Serial.println("]");
+		}
 		analogWrite(M1_EN_PIN, m1_speed);
 		analogWrite(M2_EN_PIN, m2_speed);
 	}
 }
 
-void speed_down()
+void speed_down(int verbose)
 {
 	if(m1_speed > 0)
 	{
@@ -247,12 +260,38 @@ void speed_down()
 		m2_speed = m2_speed - 1;
 		if(m1_speed < 0) m1_speed = 0;
 		if(m2_speed < 0) m2_speed = 0;
-		Serial.print("[ARDUINO] Decreasing speed: ");
-		Serial.print(((float) m1_speed * 100) / 255);
-		Serial.print("% [");
-		Serial.print(m1_speed);
-		Serial.println("]");
+		if(verbose) {
+			Serial.print("[ARDUINO] Decreasing speed: ");
+			Serial.print(((float) m1_speed * 100) / 255);
+			Serial.print("% [");
+			Serial.print(m1_speed);
+			Serial.println("]");
+		}
 		analogWrite(M1_EN_PIN, m1_speed);
 		analogWrite(M2_EN_PIN, m2_speed);
 	}
+}
+
+// Show motor pin states
+void debug_motors(int verbose)
+{
+	if(verbose)	{
+		Serial.println("[ARDUINO] Debug");
+		Serial.println("");
+	}
+
+	Serial.print("[ARDUINO] M1 EN: ");
+	Serial.println(m1_speed);
+	Serial.print("[ARDUINO] M1 A1: ");
+	Serial.println(m1_a1);
+	Serial.print("[ARDUINO] M1 A2: ");
+	Serial.println(m1_a2);
+	Serial.println("");
+	Serial.print("[ARDUINO] M2 EN: ");
+	Serial.println(m2_speed);
+	Serial.print("[ARDUINO] M2 A1: ");
+	Serial.println(m2_a1);
+	Serial.print("[ARDUINO] M2 A2: ");
+	Serial.println(m2_a2);
+	Serial.println("");
 }
