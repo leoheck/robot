72d9a0106d731c6d554a1b72a17c54d970fc182f
Leandro Heck <leoheck@gmail.com>
2014-02-26 10:14:22 -0300

    Created configs folder with usb hotplug fix

---
+++ beaglebone/configs/usb_hotplug_fix/usb_hotplug_fix.c
@@ -0,0 +1,62 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+
+int main(void) {
+
+        /* Our process ID and Session ID */
+        pid_t pid, sid;
+
+        /* Fork off the parent process */
+        pid = fork();
+        if (pid < 0) {
+                exit(EXIT_FAILURE);
+        }
+        /* If we got a good PID, then
+           we can exit the parent process. */
+        if (pid > 0) {
+                exit(EXIT_SUCCESS);
+        }
+
+        /* Change the file mode mask */
+        umask(0);
+
+        /* Open any logs here */
+
+        /* Create a new SID for the child process */
+        sid = setsid();
+        if (sid < 0) {
+                /* Log the failure */
+                exit(EXIT_FAILURE);
+        }
+
+
+
+        /* Change the current working directory */
+        if ((chdir("/")) < 0) {
+                /* Log the failure */
+                exit(EXIT_FAILURE);
+        }
+
+        /* Close out the standard file descriptors */
+        close(STDIN_FILENO);
+        close(STDOUT_FILENO);
+        close(STDERR_FILENO);
+
+        /* Daemon-specific initialization goes here */
+
+        int usbFD;
+        while(1)
+        {
+                usbFD = open("/dev/bus/usb/001/001", O_RDONLY);
+                close(usbFD);
+                sleep(1);
+        }
+   exit(EXIT_SUCCESS);
+}
